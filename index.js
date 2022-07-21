05843d86b10af8fcb6d081e343ea274679455c24
Idris Yakub <olayinkayakub@yahoo.com>
2022-07-21 20:22:33 +0100

    mongodb mongo-express

---
+++ index.js
@@ -0,0 +1,157 @@
+student= {
+    "firstName": "idris",
+    "lastName": "yakub",
+    "email": "olayinka@yahoo.com",
+    "gender": "male",
+    "country": "nigeria",
+    "isStudentActive": false,
+    "favouriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentOnBooks": 0.00
+}
+
+students= [
+    {
+    "firstName": "idris",
+    "lastName": "yakub",
+    "email": "olayinka@yahoo.com",
+    "gender": "male",
+    "country": "nigeria",
+    "isStudentActive": false,
+    "favouriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentOnBooks": 0.00
+    },
+    {
+    "firstName": "idris",
+    "lastName": "yakub",
+    "email": "olayinka@yahoo.com",
+    "gender": "male",
+    "country": "nigeria",
+    "isStudentActive": false,
+    "favouriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentOnBooks": 1.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 2.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 3.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 4.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 5.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 6.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 7.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 8.00
+    },
+    {
+        "firstName": "idris",
+        "lastName": "yakub",
+        "email": "olayinka@yahoo.com",
+        "gender": "male",
+        "country": "nigeria",
+        "isStudentActive": false,
+        "favouriteSubjects": [
+            "maths",
+            "english",
+            "it"
+        ],
+        "totalSpentOnBooks": 9.00
+    }
+]
